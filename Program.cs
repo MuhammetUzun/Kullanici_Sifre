196a632786be0d5e5dc0154a305fa9327a4bf69b
Muhammet Uzun <105195447+MuhammetUzun@users.noreply.github.com>
2022-12-07 21:45:17 +0300

    3 Hata sonrasında giriş yapılamayan algoritma

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace adminPanel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Kullanıcı adı : admin şifre : 123");
+            // 3 hatalı girişten sonra daha giremezsin :)
+            
+            var name = "admin";
+            var sifre = "123";
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Kullanıcı adını giriniz : ");
+                string ad=Console.ReadLine();
+
+                Console.WriteLine("Şifre giriniz: ");
+                string password = Console.ReadLine();
+
+                if (ad == name && sifre == password)
+                {
+                    Console.WriteLine("Kullanıcı adı ve şifre doğrudur.\nHOŞGELDİN :)");
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("Girmiş olduğunuz kullanıcı adı ve şifre yanlıştır.");
+                    if (i == 2)
+                    {
+                        Console.WriteLine("Deneme haklarınızı doldurdunuz .... \nLütfen bir yetkili ile iletişime geçiniz.");
+                    }
+                }
+
+            }       
+            
+
+        }
+    }
+}
